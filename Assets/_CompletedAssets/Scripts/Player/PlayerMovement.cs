cce6191ff0f8598670c35ff5b880dc08a7694672
aforaleka <aforaleka@gmail.com>
2017-02-09 13:16:08 -0600

    support diagonal movement, added player moving state, and fixed rotation glitch issue

--- Assets/_CompletedAssets/Scripts/Player/PlayerMovement.cs
+++ Assets/_CompletedAssets/Scripts/Player/PlayerMovement.cs
@@ -5,25 +5,29 @@ namespace CompleteProject
     public class PlayerMovement : MonoBehaviour
     {
 		public float walkSpeed = 2f;
-		public float runSpeed = 6f;
+		public float runSpeed = 5f;
 		public float slowSpeed = 1.0f;
 		float speed; // The speed that the player will move at.
 
+		Vector3 new_position;
         Vector3 movement;                   // The vector to store the direction of the player's movement.
         Animator anim;                      // Reference to the animator component.
         Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
-        int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
         float camRayLength = 100f;          // The length of the ray from the camera into the scene.
+
+		string state;
+		string idle = "idle";
+		string walk = "walk";
+		string run = "run";
+		string tiptoe = "tiptoe";
 
         void Awake ()
         {
-            // Create a layer mask for the floor layer.
-            // floorMask = LayerMask.GetMask ("Floor");
-
             // Set up references.
             anim = GetComponent <Animator> ();
             playerRigidbody = GetComponent <Rigidbody> ();
 			speed = walkSpeed;
+
         }
 
 
@@ -31,6 +35,7 @@ namespace CompleteProject
         {
             // Store the input axes.
             float v = Input.GetAxisRaw("Vertical");
+			state = v == 0f ? idle : walk;
 
 			// Set the speed according to the keys currently pressed down
 			SetSpeed ();
@@ -42,32 +47,43 @@ namespace CompleteProject
             Turning ();
 
             // Animate the player.
-            Animating (v);
+			Animating (state == walk);
         }
 
 		void SetSpeed ()
 		{
-			// Speed up to running speed
-			if (Input.GetKey (KeyCode.LeftShift))
+			if (Input.GetKey (KeyCode.LeftShift)) {
 				speed = runSpeed;
-			// Or slow down to tip-toeing
-			else if (Input.GetKey (KeyCode.Q))
+				state = run;
+			} else if (Input.GetKey (KeyCode.Q)) {
 				speed = slowSpeed;
-			
-			if (Input.GetKeyUp (KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.Q))
+				state = tiptoe;
+			} 
+
+			if (Input.GetKeyUp (KeyCode.LeftShift) || Input.GetKeyUp (KeyCode.Q)) {
 				speed = walkSpeed;
+				state = walk;
+			}
+			
 		}
 
         void Move ()
         {
-			// Go forward
-			if (Input.GetKey(KeyCode.W)) {
-				transform.position += transform.forward * Time.deltaTime * speed;
+			Vector3 delta = Vector3.zero;
+
+			if (Input.GetKey(KeyCode.W)) { 
+				delta += transform.forward;
+			}  else if (Input.GetKey(KeyCode.S)) {
+				delta -= transform.forward;
 			}
-			// Go backward
-			else if(Input.GetKey(KeyCode.S)) {
-				transform.position -= transform.forward * Time.deltaTime * speed;
+
+			if (Input.GetKey(KeyCode.D)) {
+				delta += transform.right;
+			} else if (Input.GetKey(KeyCode.A)) {
+				delta -= transform.right;
 			}
+
+			playerRigidbody.MovePosition(playerRigidbody.position + delta * Time.deltaTime * speed);
         }
 
 
@@ -79,33 +95,28 @@ namespace CompleteProject
             // Create a RaycastHit variable to store information about what was hit by the ray.
             RaycastHit floorHit;
 
-            // Perform the raycast and if it hits something on the floor layer...
-            if(Physics.Raycast (camRay, out floorHit, camRayLength, floorMask))
+            // Perform the raycast
+			if(Physics.Raycast (camRay, out floorHit, Mathf.Infinity))
             {
                 // Create a vector from the player to the point on the floor the raycast from the mouse hit.
                 Vector3 playerToMouse = floorHit.point - transform.position;
 
                 // Ensure the vector is entirely along the floor plane.
-                playerToMouse.y = 0f;
+				 playerToMouse.y = 0.0f;
 
                 // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
                 Quaternion newRotatation = Quaternion.LookRotation (playerToMouse);
 
                 // Set the player's rotation to this new rotation.
-				playerRigidbody.MoveRotation (Quaternion.Lerp(transform.rotation, newRotatation, 0.1f));
+				playerRigidbody.MoveRotation (Quaternion.Lerp(transform.rotation, newRotatation, speed/100f));
             }
         }
+			
 
-
-
-
-        void Animating (float v)
+        void Animating (bool walk)
         {
-            // Create a boolean that is true if either of the input axes is non-zero.
-            bool walking = v != 0f;
-
             // Tell the animator whether or not the player is walking.
-            anim.SetBool ("IsWalking", walking);
+            anim.SetBool ("IsWalking", walk);
         }
     }
 }
